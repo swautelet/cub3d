29806aa06d7ed93f27bb5ead19cc090367f73b9e
swautelet <swautele@student.s19.be>
2022-06-09 15:17:34 +0200

    save experience psyche

--- src/get_texture.c
+++ src/get_texture.c
@@ -6,7 +6,7 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 13:53:32 by swautele          #+#    #+#             */
-/*   Updated: 2022/06/09 15:10:35 by swautele         ###   ########.fr       */
+/*   Updated: 2022/06/09 15:17:09 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int	get_color_from_img(t_data *img, int x, int y)
 {
 	char	*dst;
 
-	dst = img->addr + (y * img->x_size + x * (img->bits_per_pixel / 8));
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	return (*(unsigned int *)dst);
 }
