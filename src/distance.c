ee75a3d59e5fe40af80573f70c4f5b6795d274f9
swautelet <swautele@student.s19.be>
2022-06-10 15:17:00 +0200

    save optimisation graphique

--- src/distance.c
+++ src/distance.c
@@ -6,7 +6,7 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 20:54:13 by simonwautel       #+#    #+#             */
-/*   Updated: 2022/06/10 15:10:55 by swautele         ###   ########.fr       */
+/*   Updated: 2022/06/10 15:15:34 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,32 +62,32 @@ double	next_vert_wall(t_param *world, double orientation, double *vert_x_wall)
 {
 	double	decal;
 	int		next_vert;
-	double	rad_orientation;
+	double	tan_orientation;
 
 	next_vert = 0;
 	decal = 0;
-	rad_orientation = degre_to_radiant(orientation);
+	tan_orientation = tan(degre_to_radiant(orientation));
 	// printf("orientation = %f\n", orientation);
 	if (orientation < 90 || orientation > 270)
 	{
 		next_vert = SIZE - (world->px_x_pos % SIZE);
-		decal = (-1 * next_vert * tan(rad_orientation));
+		decal = (-1 * next_vert * tan_orientation);
 		// printf("here\n");
 		while (check_right_wall(next_vert + world->px_x_pos, decal + world->px_y_pos, world) == FALSE)
 		{
 			next_vert += SIZE;
-			decal = (-1 * next_vert * tan(rad_orientation));
+			decal = (-1 * next_vert * tan_orientation);
 		}
 		// printf("two\n");
 	}
 	else if (orientation > 90 && orientation < 270)
 	{
 		next_vert = (world->px_x_pos % SIZE) * (-1);
-		decal = (-1 * next_vert * tan(rad_orientation));
+		decal = (-1 * next_vert * tan_orientation);
 		while (check_left_wall(next_vert + world->px_x_pos, decal + world->px_y_pos, world)== FALSE)
 		{
 			next_vert -= SIZE;
-			decal = (-1 * next_vert * tan(rad_orientation));
+			decal = (-1 * next_vert * tan_orientation);
 		}
 	// printf("next vert = %d, decal = %f\n", next_vert, decal);
 	}
@@ -102,31 +102,31 @@ double	next_hor_wall(t_param *world, double orientation, double *hor_x_wall)
 {
 	double	decal;
 	int		next_hor;
-	double	rad_orientation;
+	double	cotan_orientation;
 
 	next_hor = 0;
 	decal = 0;
-	rad_orientation = degre_to_radiant(orientation);
+	cotan_orientation = (1 / tan(degre_to_radiant(orientation)));
 	if (orientation > 0 && orientation < 180)
 	{
 		next_hor = (world->px_y_pos % SIZE) * (-1);
-		decal = -1 * (next_hor * (1 / tan(rad_orientation)));
+		decal = -1 * (next_hor * cotan_orientation);
 		// printf("look for up \n");
 		while (check_up_wall(next_hor + world->px_y_pos, decal + world->px_x_pos, world) == FALSE)
 		{
 			next_hor -= SIZE;
-			decal = -1 * (next_hor * (1 / tan(rad_orientation)));
+			decal = -1 * (next_hor * cotan_orientation);
 		}
 	}
 	else if (orientation > 180)
 	{
 		next_hor =  SIZE - (world->px_y_pos % SIZE);
-		decal = -1 * (next_hor * (1 / tan(rad_orientation)));
+		decal = -1 * (next_hor * cotan_orientation);
 		// printf("look for down \n");
 		while (check_down_wall(next_hor + world->px_y_pos, decal + world->px_x_pos, world)== FALSE)
 		{
 			next_hor += SIZE;
-			decal = -1 * (next_hor * (1 / tan(rad_orientation)));
+			decal = -1 * (next_hor * cotan_orientation);
 		}
 	}
 	*hor_x_wall = fmod((decal + world->px_x_pos), 64);
@@ -135,4 +135,3 @@ double	next_hor_wall(t_param *world, double orientation, double *hor_x_wall)
 	else
 		return (sqrt(pow(next_hor, 2) + pow(decal, 2)));
 }
-
